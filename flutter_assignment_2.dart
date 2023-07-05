6cd2aa3506a0e876544a3e799039f75aa56c0427
Abdul Rafay Bin Yousuf <98752978+Ultrafay@users.noreply.github.com>
2023-07-05 21:51:10 +0500

    flutter_assignment_2.dart

--- flutter_assignment_2.dart
+++ flutter_assignment_2.dart
@@ -114,5 +114,16 @@ if(car["isSedan"]=true && car["color"]=="Red")
 print("No match");
   }
 }
+// 10 Create a map named "person" with the following key-value pairs: "name" as "John", "age" as 25, "isStudent" as true. 
+//Write a Dart code to check if the person is both a student and over 18 years old. 
+//Print "Eligible" if both conditions are true, otherwise print "Not eligible".
+Map<String,dynamic> person ={
+  "name" : "john",
+  "age" : 25,
+  "isStudent" : true,
+};if(person["isStudent"]=true && person["age"]>18)
+{
+  print("Eligible");
+}
 
 
